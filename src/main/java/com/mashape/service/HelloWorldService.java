eaed182a788349eabf342084deb749b5baf6f31b
pansophism <pansophism@gmail.com>
2014-10-17 16:29:44 -0700

    log working

--- src/main/java/com/mashape/service/HelloWorldService.java
+++ src/main/java/com/mashape/service/HelloWorldService.java
@@ -1,10 +1,12 @@
 package com.mashape.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-
 
 /**
  * Created by yxzhao on 10/17/14.
@@ -13,9 +15,13 @@ import javax.ws.rs.core.Response;
 @Path("/hello")
 public class HelloWorldService {
 
+  private final Logger LOG = LoggerFactory.getLogger(HelloWorldService.class);
+
   @GET
   @Path("/{param}")
   public Response getMsg(@PathParam("param") String msg) {
+
+    LOG.info("User input : " + msg);
 
     String output = "Jersey say : " + msg;
 
