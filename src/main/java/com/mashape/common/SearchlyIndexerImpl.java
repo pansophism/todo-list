4991bd47de125f17965b86ca1f1143f8ab976882
pansophism <pansophism@gmail.com>
2014-10-21 20:39:45 -0700

    make search writes asynchronized

--- src/main/java/com/mashape/common/SearchlyIndexerImpl.java
+++ src/main/java/com/mashape/common/SearchlyIndexerImpl.java
@@ -7,12 +7,26 @@ import io.searchbox.client.JestClient;
 import io.searchbox.core.Delete;
 import io.searchbox.core.Index;
 import io.searchbox.core.Update;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by yxzhao on 10/20/14.
  */
 public class SearchlyIndexerImpl extends SearchlyBase implements Indexer {
+
+    private static final Logger LOG
+            = LoggerFactory.getLogger(SearchlyIndexerImpl.class);
+
+    private final AppConfig CONFIG = AppConfig.getInstance();
+
     private final JestClient client;
+    private int threadCount = CONFIG.getInt("searchly.indexer.thread.count");
+
+    private ExecutorService pool = Executors.newFixedThreadPool(threadCount);
 
     @Inject
     public SearchlyIndexerImpl(final JestClient client) {
@@ -21,26 +35,56 @@ public class SearchlyIndexerImpl extends SearchlyBase implements Indexer {
 
     @Override
     public final void update(final Task task) throws Exception {
-        Update update
-                = new Update.Builder(task).index(INDEX_NAME).type(INDEX_TYPE).build();
+        LOG.info("Updating task : " + task);
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                Update update
+                        = new Update.Builder(task).index(INDEX_NAME).type(INDEX_TYPE).build();
 
-        client.execute(update);
+                try {
+                    client.execute(update);
+                } catch (Exception e) {
+                    LOG.error("Exception : ", e);
+                }
+            }
+        });
+        ;
     }
 
     @Override
     public final void delete(final String taskID) throws Exception {
-        Delete delete
-                = new Delete.Builder(taskID).index(INDEX_NAME).type(INDEX_TYPE).build();
+        LOG.info("Deleting task by ID : " + taskID);
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                Delete delete
+                        = new Delete.Builder(taskID).index(INDEX_NAME).type(INDEX_TYPE).build();
 
-        client.execute(delete);
+                try {
+                    client.execute(delete);
+                } catch (Exception e) {
+                    LOG.error("Exception : ", e);
+                }
+            }
+        });
     }
 
     @Override
     public final void index(final Task task) throws Exception {
-        Index index
-                = new Index.Builder(task).index(INDEX_NAME).type(INDEX_TYPE).build();
-
-        client.execute(index);
+        LOG.info("Indexing task : " + task);
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                Index index
+                        = new Index.Builder(task).index(INDEX_NAME).type(INDEX_TYPE).build();
+                try {
+                    client.execute(index);
+                } catch (Exception e) {
+                    LOG.error("Exception : ", e);
+                }
+            }
+        });
     }
 
 }
